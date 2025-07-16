a0a2538c02cdff0c9118e9ab435d8837c3a8e0ef
zero0205 <etety39@naver.com>
2025-07-16 15:07:38 +0000

    :sparkles: [Feat]: requestLogIn에서 로그인을 위해 이동하는 URL에 redirectUri 추가

--- src/entities/auth/api/index.ts
+++ src/entities/auth/api/index.ts
@@ -3,7 +3,10 @@ import { devLogger } from "@/shared/lib";
 import { useAuthStore } from "@/shared/stores";
 
 export const requestLogIn = (provider: "google" | "kakao") => {
-  window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/${provider}`;
+  const redirectUri = window.location.origin;
+  const url = new URL(`${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/${provider}`);
+  url.searchParams.set("redirect_uri", redirectUri);
+  window.location.href = url.href;
 };
 
 export const requestLogOut = () => {
